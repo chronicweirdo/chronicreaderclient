70780ecd5fc8c8bac2b43095000b277344bf4ec8
Silviu Cacoveanu <caco@eml.cc>
2022-11-27 08:04:47 +0000

    getting book contents

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -1,48 +1,26 @@
-//importScripts('/libs/bookNode.js')
-//importScripts('/libs/reader.js')
-//importScripts('/libs/database.js')
-
 const DATABASE_NAME = "chronicreaderclient"
 const DATABASE_VERSION = "2"
 const FILE_TABLE = 'files'
 
-class File {
-    constructor(name, contentType, date, content) {
-        this.name = name
-        this.contentType = contentType
-        this.date = date
-        this.content = content
-    }
-}
-
-//var db
-
 function getDb() {
     return new Promise((resolve, reject) => {
-        //if (! db) {
-            const request = indexedDB.open(DATABASE_NAME, DATABASE_VERSION)
-            request.onerror = function(event) {
-                console.log("failed to open database")
-                reject()
-            }
-            request.onsuccess = function(event) {
-                //db = event.target.result
-                console.log("successfully opened database")
-                resolve(event.target.result)
-            }
-            request.onupgradeneeded = function(event) {
-                console.log("upgrading database")
-                let localDb = event.target.result
-                var imagesStore = localDb.createObjectStore(FILE_TABLE, {keyPath: 'name'})
-                resolve(localDb)
-            }
-        //} else {
-        //    resolve(db)
-        //}
+        const request = indexedDB.open(DATABASE_NAME, DATABASE_VERSION)
+        request.onerror = function(event) {
+            console.log("failed to open database")
+            reject()
+        }
+        request.onsuccess = function(event) {
+            console.log("successfully opened database")
+            resolve(event.target.result)
+        }
+        request.onupgradeneeded = function(event) {
+            console.log("upgrading database")
+            let localDb = event.target.result
+            var imagesStore = localDb.createObjectStore(FILE_TABLE, {keyPath: 'name'})
+            resolve(localDb)
+        }
     })
 }
-
-//getDb()
 
 self.addEventListener('install', e => {
     console.log("installing service worker")
@@ -51,77 +29,17 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
     var url = new URL(e.request.url)
     console.log("pathname: " + url.pathname)
-    //e.respondWith(fetch(e.request))
 
     if (url.pathname.startsWith("/upload")) {
         e.respondWith(handleUpload(e.request))
-        //e.respondWith(Response.redirect("/", 302))
     } else if (url.pathname.startsWith("/books")) {
         e.respondWith(loadAllBooks())
-    } else {
-        e.respondWith(fetch(e.request))
-    }
-
-    /*if (url.pathname.startsWith("/book")) {
-        let path = url.pathname.split("/")
-        console.log(path)
-        
+    } else if (url.pathname.startsWith("/book/")) {
         e.respondWith(loadBook(e.request))
-    } else {*/
-    /*if (url.pathname === '/markProgress') {
-        e.respondWith(handleMarkProgress(e.request))
-    } else if (url.pathname === '/loadProgress') {
-        e.respondWith(handleLoadProgress(e.request))
-    } else if (url.pathname === '/latestRead') {
-        e.respondWith(handleLatestReadRequest(e.request))
-    } else if (url.pathname === '/latestAdded') {
-        e.respondWith(handleLatestAddedRequest(e.request))
-    } else if (url.pathname === '/comic' || url.pathname === '/book') {
-        e.respondWith(handleRootDataRequest(e.request))
-    } else if (url.pathname === '/imageData' || url.pathname === '/bookResource') {
-        e.respondWith(handleDataRequest(e.request))
-    } else if (url.pathname === '/bookSection') {
-        e.respondWith(handleBookSectionRequest(e.request))
-    } else if (url.pathname === '/') {
-        e.respondWith(handleRootRequest(e.request))
-    } else if (url.pathname === '/search') {
-        e.respondWith(handleSearchRequest(e.request))
-    } else if ((url.pathname === '/login' && e.request.method == 'POST') || (url.pathname === '/logout')) {
-        e.respondWith(handleLoginLogout(e.request))
-    } else if (filesToCache.includes(url.pathname)) {
-        e.respondWith(handleWebResourceRequest(e.request))
     } else {
         e.respondWith(fetch(e.request))
-    }*/
-
-    /*    e.respondWith(fetch(e.request))
-    }*/
-})
-
-/*async function loadBook(request) {
-    try {
-        // check if book in cache
-        let db = new Database(DATABASE_NAME, DATABASE_VERSION)
-        let book = await db.load("book", "1")
-        if (book == null) {
-            // load book from server
-            let bookResponse = await fetch(request)
-            // store book to cache
-            let headers = Object.fromEntries(bookResponse.headers.entries())
-            let blob = await bookResponse.blob()
-            book = await db.save("book", "1", {"headers": headers, "body": blob})
-        }
-        return new Response(book.body, {headers: new Headers(book.headers)})
-    } catch (e) {
-        console.log(e)
     }
-}*/
-
-/*function getComicContent(id, position) {
-
-}*/
-
-
+})
 
 function get200Response() {
     return new Response("", { "status" : 200 })
@@ -168,13 +86,28 @@ async function handleUpload(request) {
 
 async function loadAllBooks() {
     let databaseBooks = await databaseLoadDistinct(FILE_TABLE, "name")
-    //let responseBooks = [] //databaseBooks.map((book) => book.name)
-    //for (let i = 0; i < databaseBooks.length; i++) {
-    //    responseBooks.push(databaseBooks[i].name)
-    //}
-    //console.log(responseBooks)
     console.log(databaseBooks)
     return getJsonResponse(Array.from(databaseBooks))
+}
+
+async function loadBook(request) {
+    let url = new URL(request.url)
+    let pathParts = url.pathname.split("/")
+    let bookId = pathParts[pathParts.length - 1]
+
+    let bookObject = await databaseLoad(FILE_TABLE, bookId)
+    if (bookObject) {
+        console.log(bookObject)
+        console.log(typeof bookObject.content)
+        const hdrs = new Headers()
+        hdrs.append('Content-Type', bookObject.contentType)
+        return new Response(bookObject.content, {
+            status: 200,
+            headers: hdrs
+        })
+    } else {
+        return get404Response()
+    }
 }
 
 function databaseSave(table, value) {
