d06ec2992fd51cd5a094b17c77d8bd0b852e9cb8
Silviu Cacoveanu <caco@eml.cc>
2023-01-04 12:04:57 +0200

    different url

--- serviceworkerinstall.js
+++ serviceworkerinstall.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.getRegistrations().then(registrations => {
-        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+        navigator.serviceWorker.register('/chronicreaderclient/serviceworker.js').then(function(registration) {
             // we load latest read both on success and on failure (when offline)
             registration.update().then(() => console.log("installed service worker"))
         }, function(error) {
